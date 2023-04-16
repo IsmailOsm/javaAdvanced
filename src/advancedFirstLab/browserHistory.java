48ef96bbee43c92ae8e3af12971c16b8639aaceb
IsmailOsm <117646028+IsmailOsm@users.noreply.github.com>
2023-04-16 19:07:50 +0200

    First problem solution.

---
+++ src/advancedFirstLab/browserHistory.java
@@ -0,0 +1,29 @@
+package advancedFirstLab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class browserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        while (!input.equals("Home")) {
+            if (input.equals("back")) {
+                if (stack.isEmpty() || stack.size() == 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+
+            } else {
+                stack.push(input);
+                System.out.println(input);
+            }
+
+
+            input = scanner.nextLine();
+        }
+    }
+}
