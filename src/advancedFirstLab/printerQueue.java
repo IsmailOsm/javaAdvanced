a23f854edbe1f5350e1acf1cddd55accec66f6a0
IsmailOsm <117646028+IsmailOsm@users.noreply.github.com>
2023-04-17 13:21:27 +0200

    Solutions

---
+++ src/advancedFirstLab/printerQueue.java
@@ -0,0 +1,30 @@
+package advancedFirstLab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class printerQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        ArrayDeque<String> queue = new ArrayDeque<>();
+        while (!fileName.equals("print")) {
+            if (fileName.equals("cancel")){
+                if (queue.isEmpty()){
+                    System.out.println("Printer is on standby");
+                }else {
+                    System.out.printf("Canceled %s%n",queue.peek());
+                    queue.pollFirst();
+                }
+
+            } else {
+                queue.offer(fileName);
+            }
+
+            fileName = scanner.nextLine();
+        }
+        while (!queue.isEmpty()){
+            System.out.println(queue.pollFirst());
+        }
+    }
+}
