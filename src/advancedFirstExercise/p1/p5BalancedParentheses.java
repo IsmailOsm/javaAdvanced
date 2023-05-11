14ccea7d308d2526a53d2978760de01db51e7118
IsmailOsm <117646028+IsmailOsm@users.noreply.github.com>
2023-05-11 18:27:41 +0200

    Solutions

---
+++ src/advancedFirstExercise/p1/p5BalancedParentheses.java
@@ -0,0 +1,65 @@
+package advancedFirstExercise.p1;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class p5BalancedParentheses {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        boolean flag = true;
+        if (string.length() % 2 == 1){
+
+        }
+            for (int i = 0; i < string.length(); i++) {
+                if (string.length() % 2 == 1){
+                    System.out.println("NO");
+                    flag = false;
+                    break;
+                }
+                char symbol = string.charAt(i);
+                if (symbol == '{' || symbol == '[' || symbol == '(') {
+                    stack.push(i);
+                } else {
+                    int index = i;
+                    char closingBracket = string.charAt(i);
+                    char openingBracket = string.charAt(stack.peek());
+                    stack.pop();
+                    if (openingBracket == '(') {
+                        if (closingBracket == ')') {
+
+                        } else {
+                            System.out.println("NO");
+                            flag = false;
+                            break;
+                        }
+
+                    } else if (openingBracket == '{') {
+                        if (closingBracket == '}') {
+
+                        } else {
+                            System.out.println("NO");
+                            flag = false;
+                            break;
+                        }
+
+                    } else if (openingBracket == '[') {
+                        if (closingBracket == ']') {
+
+                        } else {
+                            System.out.println("NO");
+                            flag = false;
+                            break;
+                        }
+
+                    }
+
+
+                }
+            }
+        if (flag) {
+            System.out.println("YES");
+        }
+    }
+}
