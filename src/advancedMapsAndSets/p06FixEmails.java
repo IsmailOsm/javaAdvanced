44a9774d9e83fef74367dd3cbad0662bb05df1ba
IsmailOsm <117646028+IsmailOsm@users.noreply.github.com>
2023-05-22 11:54:36 +0200

    Solutions

--- src/advancedMapsAndSets/p06FixEmails.java
+++ src/advancedMapsAndSets/p06FixEmails.java
@@ -1,10 +1,22 @@
 package advancedMapsAndSets;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class p06FixEmails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        Map<String, String> map = new LinkedHashMap<>();
+        String input = scanner.nextLine();
+        while (!input.equals("stop")) {
+            String name = input;
+            String email = scanner.nextLine();
+            if (!email.endsWith("com") && !email.endsWith("uk") && !email.endsWith("us")) {
+                map.put(name, email);
+            }
+            input = scanner.nextLine();
+        }
+        map.entrySet().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
     }
 }
---
+++ src/advancedMapsAndSets/p07HandsOfCards.java
@@ -0,0 +1,53 @@
+package advancedMapsAndSets;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class p07HandsOfCards {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Map<String, Integer>> map = new LinkedHashMap<>();
+        String input = scanner.nextLine();
+        while (!input.equals("JOKER")) {
+            String name = input.split(": ")[0];
+            String[] cards = input.split(": ")[1].split(", ");
+            if (!map.containsKey("name")) {
+                map.put(name, new HashMap<>());
+            }
+            Map<String, Integer> secondMap = new HashMap<>();
+            for (int i = 0; i < cards.length; i++) {
+                String card = cards[i];
+                int cardPower = 1;
+                int cardType = 1;
+                if (card.endsWith("S")) {
+                    cardType = 4;
+                } else if (card.endsWith("H")) {
+                    cardType = 3;
+                } else if (card.endsWith("D")) {
+                    cardType = 2;
+                }
+                if (card.startsWith("10")) {
+                    cardPower = 10;
+                } else if (card.startsWith("J")) {
+                    cardPower = 11;
+                } else if (card.startsWith("Q")) {
+                    cardPower = 12;
+                } else if (card.startsWith("K")) {
+                    cardPower = 13;
+                } else if (card.startsWith("A")) {
+                    cardPower = 14;
+                } else {
+                    cardPower = Integer.parseInt("" + card.toCharArray()[0]);
+                }
+                int totalPower = cardPower * cardType;
+                secondMap.put(card, totalPower);
+            }
+            map.put(name, secondMap);
+
+            input = scanner.nextLine();
+        }
+
+    }
+}
